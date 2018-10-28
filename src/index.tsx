6e555f7328e704a50c135742b0cee1492d165683
Florian Eckerstorfer <florian@eckerstorfer.net>
2018-10-28 17:09:25 +0100

    Persist user state

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,19 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './components/App/App';
 import configureStore from './configureStore';
 import './index.scss';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = configureStore();
+const { store, persistor } = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root') as HTMLElement,
 );
